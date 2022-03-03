6d162cffca6644bcd4b4330d304b8bdb7ff88f15
IanBarnes96 <74927667+IanBarnes96@users.noreply.github.com>
2022-03-03 09:17:02 +0000

    Disjoint
    Started writing disjoint methods in controller and started rectangle methods

--- src/main/java/com/example/datastructures2ca1/PCBAnalyserController.java
+++ src/main/java/com/example/datastructures2ca1/PCBAnalyserController.java
@@ -6,7 +6,9 @@ import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.*;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
@@ -14,6 +16,7 @@ import java.io.File;
 
 
 public class PCBAnalyserController {
+
     @FXML
     private ImageView mainImage;
     @FXML
@@ -23,6 +26,11 @@ public class PCBAnalyserController {
     private Label Brightness1, Saturation1, Hue1, Red1, Green1, Blue1;
 
     private PixelReader preader;
+
+    Rectangle rect;
+
+    int[] djs;
+    int[] objects;
 
 
     public void openImage(ActionEvent ActionEvent) {
@@ -44,6 +52,7 @@ public class PCBAnalyserController {
     }
 
     public void clickedImage(MouseEvent event) {
+
         preader = mainImage.getImage().getPixelReader();
         Image inputImage = mainImage.getImage();
 
@@ -86,18 +95,33 @@ public class PCBAnalyserController {
 
                     if (blackIdentify(red, green, blue, hue, redValue, greenValue, blueValue, hueValue)) {
                         pw.setColor(w, h, Color.color(0, 0, 0));
+
+
                     } else {
                         pw.setColor(w, h, Color.color(1, 1, 1));
+
                     }
                 }
             }
             image2.setImage(writableImage);
+
+            drawRectangle();
         });
     }
 
     private static boolean blackIdentify(Double red, Double green, Double blue, Double hue, Double redVal, Double greenVal, Double blueVal, Double hueVal) {
         return red > redVal - 0.15 && red < redVal + 0.15 && green > greenVal - 0.15 && green < greenVal + 0.15 && blue > blueVal - 0.15 &&
                 blue < blueVal + 0.15 && hue > hueVal - 3 && hue < hueVal + 3;
+    }
+
+    public boolean blackPixels(Color color){
+        double average = (color.getRed() + color.getGreen() + color.getBlue() /3);
+        if(average <= 0.5){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     /**
@@ -116,6 +140,75 @@ public class PCBAnalyserController {
     public static int findRec(int[] a, int id){
         return a[id] == id ? id : findRec(a,a[id]);
     }
+
+    public void union(int[] a, int p, int q){
+        a[find(a,q)] = find(a,p);
+    }
+
+    public void drawRectangle(){
+        int selectedObjects = 0;
+
+        for(int id = 0; id < objects.length; id ++){
+
+            selectedObjects ++;
+            double maxHeight = -1;
+            double minHeight = -1;
+            double left = -1;
+            double right = -1;
+             for(int i = 0; i < djs.length; i ++){
+                 int x = i % (int) mainImage.getFitWidth();
+                 int y = i / (int) mainImage.getFitWidth();
+
+                 if(djs[i] != -1 && find(djs, i) == id){
+                     if(maxHeight == -1){
+                         maxHeight = minHeight = y;
+                         left = right = x;
+
+                     }
+
+                 }
+             }
+            Rectangle rect = new Rectangle(left,maxHeight, right-left,minHeight -maxHeight);;
+            rect.setStrokeWidth(1);
+            rect.setStroke(Color.CORNFLOWERBLUE);
+            rect.setFill(Color.TRANSPARENT);
+
+
+            ((AnchorPane) mainImage.getParent()).getChildren().add(rect);
+        }
+
+    }
+
+
+/*    public void ColorSelected(){
+        for(int y = 0; y < image2.getFitHeight();y++){
+            for(int x = 0; x < image2.getFitWidth(); x++) {
+                Color col = image2.getImage().getPixelReader().getColor(x, y);
+                if (blackPixels(col)) {
+                    Rectangle(x, y);
+                }
+            }
+            }
+        }*/
+
+        public void createDisjointSet(){
+        int width = (int) mainImage.getFitWidth();
+        for(int i =0; i < djs.length; i ++){
+            if(djs[i] !=0){
+                if((i + 1) % width !=0 && djs[i +1] != 0){
+                    union(djs , i , i + 1);
+                }
+                if(i +width < djs.length && djs[i + width] != 0){
+                    union(djs, i, i + width);
+                }
+                else{
+                    djs[i] = 0;
+                }
+                i ++;
+            }
+        }
+        }
+
 
 
 }
